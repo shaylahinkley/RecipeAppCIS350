313d06d34a6530999079ed7ef789cfa10bfb3153
shaylahinkley <hinkleys@mail.gvsu.edu>
2020-04-11 23:13:03 -0400

    in the search section, I have added a pop up window that appears when you click the recipe in the list. This is where we will put instructions.

---
+++ app/src/main/java/recipe/app/Pop.java
@@ -0,0 +1,26 @@
+package recipe.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+/**
+ * Class that allows pop up windows to be used when in the search section of the app. This class controls the aspects
+ * of the screen when an item from the searched item list is pressed
+ */
+public class Pop extends Activity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.searchpopwindow);
+
+        DisplayMetrics dm =  new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*0.8), (int) (height * 0.6));
+    }
+}
